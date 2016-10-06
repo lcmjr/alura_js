f8d1f476a9badf06e2d171f166aae6ad1f78be46
Luiz Carlos <luiz.manhani@gmail.com>
2016-10-06 13:37:07 -0300

    class 2-2 (create the class negociacao it will be the model)

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,10 @@
+/**
+ * Created by luizm on 06/10/2016.
+ */
+class Negociacao {
+    constructor(){
+        this.data = new Date();
+        this.quantidade = 1;
+        this.valor = 0.0;
+    }
+}
