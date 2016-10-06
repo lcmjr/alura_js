1e67cf779bff4feec2f78dad7fe228d138d6d546
Luiz Carlos <luiz.manhani@gmail.com>
2016-10-06 13:45:31 -0300

    class 2-3 (create obtemVolume that returns the quantity * price of a negotiation)

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -2,9 +2,14 @@
  * Created by luizm on 06/10/2016.
  */
 class Negociacao {
-    constructor(){
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+
+    constructor( data, quantidade, valor){
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    obtemVolume(){
+        return this.valor * this.quantidade;
     }
 }
