cdaafcbbf0ae18876364d99a38f2ccaffb567b5e
Luiz Carlos <luiz.manhani@gmail.com>
2016-11-07 09:38:08 -0200

    creating datehelper

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,18 +11,15 @@ class NegociacaoController{
     adiciona(event){
         event.preventDefault();
 
-        /*
-        ... = para retornar o array na ordem como parametro ex: array[0] parametro[0]
-        => = arrow function não se torna nescessário escrever function,
-        com somente uma instrução não precisa de bloco {} e ele ja retornara o resultado
-        0%1=0, 1%2=1 2%2=0 assim se evita o erro para corrigir o array de data*/
-        let data = new Date(...this._inputdata.value.split('-')
-            .map((item,indice) => item - indice % 2));
-                
+        let helper = new DateHelper();
+        let data = helper.textoParaData(this._inputdata.value);
+
         let negociacao = new Negociacao(
             data,
             this._inputquantidade.value,
             this._inputvalor.value);
         console.log(negociacao);
+
+        console.log(helper.dataParaTexto(negociacao.data));
     }
 }
