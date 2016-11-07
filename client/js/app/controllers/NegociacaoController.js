51ed65cfab1f853600dbe8fd9d5d9062be85b406
Luiz Carlos <luiz.manhani@gmail.com>
2016-11-07 11:22:40 -0200

    creating model ListaNegociacoes In controller create a method that add a negociacao and another that  reset the form

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,19 +6,30 @@ class NegociacaoController{
         this._inputdata = $("#data");
         this._inputquantidade = $("#quantidade");
         this._inputvalor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
         event.preventDefault();
 
-        let data = DateHelper.textoParaData(this._inputdata.value);
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
 
-        let negociacao = new Negociacao(
-            data,
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputdata.value),
             this._inputquantidade.value,
             this._inputvalor.value);
-        console.log(negociacao);
+    }
 
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+    _limpaFormulario(){
+        this._inputdata.value = '';
+        this._inputquantidade.value = 1;
+        this._inputvalor.value = 0.0;
+
+        this._inputdata.focus();
     }
 }
